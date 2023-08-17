98eb4bf111f053a2eff634e456dd839569fa5da9
Bruna Santiago <brunasanthiago@outlook.com>
2023-08-17 14:17:37 -0300

    First commit

---
+++ cypress/e2e/formularioCadastro.cy.js
@@ -0,0 +1,6 @@
+describe('Formulário de Cadastro', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+    
+})
---
+++ cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,20 @@
+describe('Formulário de Login', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+    it('Não dever permitir um email inválido', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('brunasantiago@alura')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+
+        cy.getByData('mensagem-erro').should('contain', 'O email digitado é inválido')
+    })
+    it('Não deve permitir um campo em branco', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+
+        cy.getByData('mensagem-erro').should('contain', 'O campo email é obrigatório')
+    })
+})
